5527ea568e2721ae356fd585b12e0c8400129336
winton <mail@wintoni.us>
2020-03-03 22:21:22 -0800

    Ensure each test wrapped in describe

--- tsignore/router.spec.ts
+++ tsignore/router.spec.ts
@@ -5,17 +5,19 @@ import logger from "../src/logger"
 import router from "../src/router"
 import tinyId from "../src/tinyId"
 
-beforeEach(() => {
-  loaded.reset()
-  loaded.load({ logger, router, tinyId })
-})
-
-it("routes", () => {
-  router.add({
-    404: () => "404",
-    route: () => "route",
+describe("router", () => {
+  beforeEach(() => {
+    loaded.reset()
+    loaded.load({ logger, router, tinyId })
   })
 
-  expect(router.route("/route")).toBe("route")
-  expect(router.route("/no-exist")).toBe("404")
+  it("routes", () => {
+    router.add({
+      404: () => "404",
+      route: () => "route",
+    })
+
+    expect(router.route("/route")).toBe("route")
+    expect(router.route("/no-exist")).toBe("404")
+  })
 })
